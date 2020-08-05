d7a580dc8fe9ed52ba3ba45e1a8813d90be4575c
orzogc <orzogc@163.com>
2020-08-05 09:04:13 +0800

    Rename server.go to webapi.go

--- webui.go
+++ webui.go
@@ -6,17 +6,17 @@ import (
 	"time"
 )
 
-// web ui html和js文件所在位置
+// web UI html和js文件所在位置
 const webUIDir = "webui"
 
 var uiSrv *http.Server
 
-// web ui服务器
+// web UI服务器
 func webUI() {
 	defer func() {
 		if err := recover(); err != nil {
 			lPrintErr("Recovering from panic in webUI(), the error is:", err)
-			lPrintErr("web ui服务器发生错误，尝试重启web ui服务器")
+			lPrintErr("web UI服务器发生错误，尝试重启web UI服务器")
 			time.Sleep(2 * time.Second)
 			go webUI()
 		}
